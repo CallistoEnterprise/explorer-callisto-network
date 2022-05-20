ca567f0d35323045b3fa82b3f9b012add0655f53
Erik <esnetto@gmail.com>
2022-05-20 04:15:39 -0300

    Callisto customization

--- apps/explorer/config/config.exs
+++ apps/explorer/config/config.exs
@@ -11,7 +11,7 @@ disable_webapp = System.get_env("DISABLE_WEBAPP")
 # General application configuration
 config :explorer,
   ecto_repos: [Explorer.Repo],
-  coin: System.get_env("COIN") || "POA",
+  coin: "CLO",
   coingecko_coin_id: System.get_env("COINGECKO_COIN_ID"),
   token_functions_reader_max_retries: 3,
   allowed_evm_versions:
